27cd3a11503c4272506bd50c0de4ec6d3b526666
Vallermosa20 <angel.vallermosa@gmail.com>
2023-10-16 09:57:13 +0300

    add movies

--- BSIT3L-Movies/Controllers/HomeController.cs
+++ BSIT3L-Movies/Controllers/HomeController.cs
@@ -8,9 +8,29 @@ public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
 
+    private readonly List<MovieViewModel> _movies;
+
     public HomeController(ILogger<HomeController> logger)
     {
         _logger = logger;
+        _movies = new List<MovieViewModel>
+        {
+            new MovieViewModel { Id = 1, Name = "Titanic", rating = "5", ReleaseYear = 1997, Genre = "Romance/Drama", image = }
+            new MovieViewModel { Id = 2, Name = "Inception", rating = "4", ReleaseYear = 2010, Genre = "Science Fiction/Thriller", image = }
+            new MovieViewModel { Id = 3, Name = "The Shawshank Redemption", rating = "5", ReleaseYear = 1994, Genre = "Drama", image = }
+            new MovieViewModel { Id = 4, Name = "Nanny McPhee", rating = "4", ReleaseYear = 2005, Genre = "Fantasy", image = }
+            new MovieViewModel { Id = 5, Name = "Matilda", rating = "4", ReleaseYear = 1996, Genre = "Fantasy", image = }
+            new MovieViewModel { Id = 6, Name = "Luck", rating = "4", ReleaseYear = 2002, Genre = "Comedy", image = }
+            new MovieViewModel { Id = 7, Name = "The Witches", rating = "3", ReleaseYear = 2020, Genre = "Comedy", image = }
+            new MovieViewModel { Id = 8, Name = "Love at First Sight", rating = "4", ReleaseYear = 2023, Genre = "Romance/Drama", image = }
+            new MovieViewModel { Id = 9, Name = "Persuasion", rating = "2", ReleaseYear = 2022, Genre = "Romance/Drama", image = }
+            new MovieViewModel { Id = 10, Name = "After We fell", rating = "3", ReleaseYear = 2021, Genre = "Romance/Drama", image = }
+            new MovieViewModel { Id = 11, Name = "The Invitation", rating = "2", ReleaseYear = 2022, Genre = "Horror", image = }
+            new MovieViewModel { Id = 12, Name = "Incantation", rating = "3", ReleaseYear = 2022, Genre = "Horror", image = }
+            new MovieViewModel { Id = 13, Name = "True Beauty", rating = "4", ReleaseYear = 2020, Genre = "Romance/Drama", image = }
+            new MovieViewModel { Id = 14, Name = "Train to Busan", rating = "5", ReleaseYear = 2016, Genre = "Horror", image = }
+            new MovieViewModel { Id = 15, Name = "The 8th Night", rating = "3", ReleaseYear = 2021, Genre = "Horror", image = }
+        }
     }
 
     public IActionResult Index()
--- BSIT3L-Movies/Controllers/MoviesController.cs
+++ BSIT3L-Movies/Controllers/MoviesController.cs
@@ -6,15 +6,28 @@ namespace BSIT3L_Movies.Controllers
 {
 	public class MoviesController : Controller
     {
-        private List<Movie> _movies;
+        private List<MovieViewModel> _movies;
+
         public MoviesController()
 		{
             // Sample movie data
-            _movies = new List<Movie>
+            _movies = new List<MovieViewModel>
             {
-                new Movie { Id = 1, Name = "Titanic", Rating = "5" },
-                new Movie { Id = 2, Name = "Inception", Rating = "4" },
-                new Movie { Id = 3, Name = "The Shawshank Redemption", Rating = "5" }
+                new MovieViewModel { Id = 1, Name = "Titanic", rating = "5", ReleaseYear = 1997, Genre = "Romance/Drama" },
+            new MovieViewModel { Id = 2, Name = "Inception", rating = "4", ReleaseYear = 2010, Genre = "Science Fiction/Thriller" },
+            new MovieViewModel { Id = 3, Name = "The Shawshank Redemption", rating = "5", ReleaseYear = 1994, Genre = "Drama" },
+            new MovieViewModel { Id = 4, Name = "Nanny McPhee", rating = "4", ReleaseYear = 2005, Genre = "Fantasy" },
+            new MovieViewModel { Id = 5, Name = "Matilda", rating = "4", ReleaseYear = 1996, Genre = "Fantasy" },
+            new MovieViewModel { Id = 6, Name = "Luck", rating = "4", ReleaseYear = 2002, Genre = "Comedy" },
+            new MovieViewModel { Id = 7, Name = "The Witches", rating = "3", ReleaseYear = 2020, Genre = "Comedy" },
+            new MovieViewModel { Id = 8, Name = "Love at First Sight", rating = "4", ReleaseYear = 2023, Genre = "Romance/Drama" },
+            new MovieViewModel { Id = 9, Name = "Persuasion", rating = "2", ReleaseYear = 2022, Genre = "Romance/Drama" },
+            new MovieViewModel { Id = 10, Name = "After We fell", rating = "3", ReleaseYear = 2021, Genre = "Romance/Drama" },
+            new MovieViewModel { Id = 11, Name = "The Invitation", rating = "2", ReleaseYear = 2022, Genre = "Horror" },
+            new MovieViewModel { Id = 12, Name = "Incantation", rating = "3", ReleaseYear = 2022, Genre = "Horror" },
+            new MovieViewModel { Id = 13, Name = "True Beauty", rating = "4", ReleaseYear = 2020, Genre = "Romance/Drama" },
+            new MovieViewModel { Id = 14, Name = "Train to Busan", rating = "5", ReleaseYear = 2016, Genre = "Horror" },
+            new MovieViewModel { Id = 15, Name = "The 8th Night", rating = "3", ReleaseYear = 2021, Genre = "Horror" },
             };
         }
         public ActionResult Random()
